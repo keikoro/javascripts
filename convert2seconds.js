d476e345a2de18f9d5e858865e75634c31073244
Kerstin Kollmann <code@kerstinkollmann.com>
2016-09-25 03:15:01 +0200

    convert seconds back to hrs/mins

--- convert2seconds.js
+++ convert2seconds.js
@@ -1,15 +1,28 @@
 function calc() {
-    var hh = document.getElementById('hh').value;
-    var mm = document.getElementById('mm').value;
-    var ss = document.getElementById('ss').value;
+    var hh = Number(document.getElementById('hh').value);
+    var mm = Number(document.getElementById('mm').value);
+    var ss = Number(document.getElementById('ss').value);
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
     var display;
     var calculate;
 
     // display = hh + ':' + mm + ':' + ss
     // console.log(display);
 
-    calculate = Number(hh) * 60 * 60 + Number(mm) * 60 + Number(ss);
-    document.getElementById('result').innerHTML = calculate + " seconds";
+    if (hh == 0 && mm == 0 && ss >= 60) {
+        minutes = Math.floor(ss/60);
+        seconds = Math.round(((ss/60) - minutes) * 60);
+        if (minutes >= 60) {
+            hours = minutes/60;
+            minutes = minutes/60 - hours;
+        }
+        document.getElementById('result').innerHTML = hours + " hour(s) " + minutes + " minute(s) " + seconds + " second(s)";
+    } else {
+        calculate = hh * 60 * 60 + mm * 60 + ss;
+        document.getElementById('result').innerHTML = calculate + " seconds";
+    }
 
     // console.log(calculate);
 
