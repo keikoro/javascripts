de2acc1112312f55bf0b74b3de88009236ee6a7f
K Kollmann <git@k.kollmann.moe>
2016-10-12 13:25:11 +0200

    calculate reserved/occupied space + resize column(s) accordingly

--- keycodes.js
+++ keycodes.js
@@ -1,10 +1,10 @@
+var cellHeight;
+
 // code to execute on key presses
 window.addEventListener("keypress", keyboardInput, false);
 
 
 function keyboardInput(event) {
-/*    var keycode = event.keyCode;
-    console.log(keycode);*/
 
     // get Unicode for key presses
     var keyCode = event.which;
@@ -13,12 +13,18 @@ function keyboardInput(event) {
 
     // create a new div for each key press and append it to the body
     var makeDiv = document.createElement("div");
-    makeDiv.className = "fun";
+    makeDiv.className = "regular";
 
-    text = document.createTextNode("" + thisKey + " " + keyCode);
+    // make spaces show up
+    if (keyCode == 32) {
+        thisKey = "\u00A0";
+    }
+
+    var text = document.createTextNode("" + thisKey + " " + keyCode);
     makeDiv.appendChild(text);
 
-    document.body.appendChild(makeDiv);
+    var column = document.getElementsByClassName('column')[0];
+    column.appendChild(makeDiv);
 }
 
 // get the current browser window size
@@ -28,13 +34,49 @@ function calc_size() {
         d = document,
         e = d.documentElement,
         g = d.getElementsByTagName('body')[0],
-        height = w.innerWidth || e.clientWidth || g.clientWidth,
-        width = w.innerHeight || e.clientHeight || g.clientHeight;
+        width = w.innerWidth || e.clientWidth || g.clientWidth,
+        height = w.innerHeight || e.clientHeight || g.clientHeight;
 
-    console.log("height: " + height + "px, width: " + width + "px");
+    console.log("h: " + height + "px\nw: " + width + "px");
     return [height, width];
 }
 
+
+// add height of any header-like elements on page together and return it
+function getOccupiedSpace() {
+
+    var headerHeight = document.getElementById('header').offsetHeight;
+    var footerHeight = document.getElementById('footer').offsetHeight;
+    var spacerHeight = document.getElementsByClassName('spacer')[0].offsetHeight;
+
+
+    var occupiedHeight = headerHeight + footerHeight + spacerHeight + 10;
+    console.log("occupied h: " + occupiedHeight + "px");
+
+    return occupiedHeight;
+}
+
+// change height of columns depending on document height
+function resizeColumns(height) {
+
+    // subtract height occupied by header to calculate remaining height
+    var occupiedHeight = getOccupiedSpace();
+    var newHeight = height - occupiedHeight;
+    console.log("remaining h: " + newHeight + "px");
+
+    // calculate how many cells fit into one column
+    // and base new column height on it
+    var noOfCells = Math.floor(newHeight/cellHeight) - 1;
+    var newColHeight = (noOfCells*cellHeight) + 10;
+
+    // iterate over columns to change their height
+    var column = document.getElementsByClassName('column');
+    var countCols = column.length;
+    for (var i=0; i<countCols; i++) {
+        document.getElementsByClassName('column')[i].style.height
+            = newColHeight + "px";
+    }
+}
 
 // code to execute once the site has finished loading
 document.addEventListener('DOMContentLoaded', function() {
@@ -43,20 +85,21 @@ document.addEventListener('DOMContentLoaded', function() {
     var height = window_size[0];
     var width = window_size[1];
 
-    // get coordinates of any header-like elements on page
-    // (new content should start below these elements)
-    var occupied = document.getElementsByClassName('spacer')[0];
-    var rect = occupied.getBoundingClientRect();
-    console.log(rect.top, rect.right, rect.bottom, rect.left);
+    // determine height of one cell
+    cellHeight = document.getElementsByClassName('regular')[0].offsetHeight;
+    console.log("cell h: " + cellHeight + "px");
 
-    var new_top = rect.bottom
-
-    // subtract height of header-like elements
-    // to calculate measurements of remaining available space
-    var new_height = height - (rect.bottom - rect.top);
-    console.log("new height: " + new_height + "px");
+    resizeColumns(height);
 });
 
 
 // code to execute on browser window resize
-window.addEventListener("resize", calc_size);
+window.addEventListener('resize', function() {
+
+    var window_size = calc_size();
+    var height = window_size[0];
+    var width = window_size[1];
+
+    resizeColumns(height);
+});
+
